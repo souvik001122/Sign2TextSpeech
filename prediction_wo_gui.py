6f042c27a282ce4cb2427caf002551864bf4ed84
souvik001122 <souvikdas787286@gmail.com>
2025-11-12 23:41:19 +0530

    fix: replace hard-coded paths with relative paths in prediction_wo_gui

--- prediction_wo_gui.py
+++ prediction_wo_gui.py
@@ -4,10 +4,15 @@ from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 from keras.models import load_model
 import traceback
+import os
 
-model = load_model('/cnn8grps_rad1_model.h5')
+# Use relative paths
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'cnn8grps_rad1_model.h5')
+WHITE_IMAGE_PATH = os.path.join(os.path.dirname(__file__), 'white.jpg')
+
+model = load_model(MODEL_PATH)
 white = np.ones((400, 400), np.uint8) * 255
-cv2.imwrite("C:\\Users\\devansh raval\\PycharmProjects\\pythonProject\\white.jpg", white)
+cv2.imwrite(WHITE_IMAGE_PATH, white)
 
 capture = cv2.VideoCapture(0)
 
@@ -44,7 +49,7 @@ while True:
             hand = hands[0]
             x, y, w, h = hand['bbox']
             image = frame[y - offset:y + h + offset, x - offset:x + w + offset]
-            white = cv2.imread("C:\\Users\\devansh raval\\PycharmProjects\\pythonProject\\white.jpg")
+            white = cv2.imread(WHITE_IMAGE_PATH)
             # img_final=img_final1=img_final2=0
             handz = hd2.findHands(image, draw=False, flipType=True)
             if handz:
